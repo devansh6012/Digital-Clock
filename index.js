769e5ee35fa96a5be7797fa2ebb96f7b2827f09f
Devansh Gupta <devansh.g6012@gmail.com>
2022-01-07 13:38:28 +0530

    Making the digit tick

--- index.js
+++ index.js
@@ -2,8 +2,101 @@ const container = d3.select('.container')
 
 const svg = container
     .append('svg')
-    .attr('height',window.innerHeight)
-    .attr('weight',window.innerWidth);
+    .attr('height', window.innerHeight)
+    .attr('weight', window.innerWidth);
+
+let digits = [
+    [
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+    ],
+    [
+        [1, 1, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+        [1, 1, 1],
+        [1, 0, 0],
+        [1, 0, 0],
+        [1, 1, 1],
+    ],
+    [
+        [1, 1, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+        [1, 1, 1],
+    ],
+    [
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+    ],
+    [
+        [1, 1, 1],
+        [1, 0, 0],
+        [1, 0, 0],
+        [1, 1, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+        [1, 1, 1],
+    ],
+    [
+        [1, 1, 1],
+        [1, 0, 0],
+        [1, 0, 0],
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+    ],
+    [
+        [1, 1, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+    ],
+    [
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+    ],
+    [
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 0, 1],
+        [1, 1, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+        [0, 0, 1],
+    ],
+];
 
 let squareData = {
     height: 45,
@@ -14,35 +107,42 @@ let squareData = {
     margin: 5
 }
 
-let digitData = [];
-
 let makeDigit = () => {
     let digitData = [];
     // i -> rows
     // j -> columns
-    for(let i = 0; i < 7; i++){
-        for(let j = 0; j < 3; j++){
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 3; j++) {
             // 21 digits
-            let currSquare = {...squareData};
-            
-            if(j == 1 && i > 0 && i < 6) currSquare.color = 'white';
+            let currSquare = { ...squareData };
+
+            if (j == 1 && i > 0 && i < 6) currSquare.color = 'white';
 
             currSquare.x = j * (currSquare.width + currSquare.margin);
             currSquare.y = i * (currSquare.width + currSquare.margin);
 
             digitData.push(currSquare)
-        }    
+        }
     }
 
     return digitData;
 }
 
+let digitData = makeDigit();
 // Step 1 : Make a Square
 
 let render = () => {
-    let digitData = makeDigit();
-    svg.selectAll('rect').
-        data(digitData)
+    let rects = svg
+        .selectAll('rect')
+        .data(digitData)
+        .attr('height', d => d.height)
+        .attr('width', d => d.width)
+        .attr('fill', d => d.color)
+        .attr('x', d => d.x)
+        .attr('y', d => d.y)
+        .attr('margin', d => d.margin)
+
+    rects
         .enter()
         .append("rect")
         .attr('height', d => d.height)
@@ -53,14 +153,28 @@ let render = () => {
         .attr('margin', d => d.margin)
 }
 
-render();
+let updateDigit = (s) => {
+    for (let i = 0; i < digitData.length; i++) {
+        // square object => color
+        let r = Math.floor(i / 3);
+        let c = i % 3;
 
-// setInterval(() => {
-//     // extract time
-//     let time = new Date();
-//     let h = time.getHours();
-//     let m = time.getMinutes();
-//     let s = time.getSeconds();
+        if (digits[s][r][c] === 1) {
+            digitData[i].color = 'blue';
+        }
+        else {
+            digitData[i].color = 'white';
+        }
+    }
+}
 
-    
-// }, 1000)
+setInterval(() => {
+    // extract time
+    let time = new Date();
+    let h = time.getHours();
+    let m = time.getMinutes();
+    let s = time.getSeconds();
+
+    updateDigit(s % 10);
+    render()
+}, 1000)
